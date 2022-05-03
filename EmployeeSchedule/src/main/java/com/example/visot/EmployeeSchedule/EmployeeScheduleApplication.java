bab020fbfcb983a061c1ab617fdc230364a21372
Visoth <blacksterteoz@gmail.com>
2022-05-02 17:20:44 -0700

    Initial commit

---
+++ EmployeeSchedule/src/main/java/com/example/visot/EmployeeSchedule/EmployeeScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.visot.EmployeeSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeScheduleApplication.class, args);
+	}
+
+}
---
+++ EmployeeSchedule/src/test/java/com/example/visot/EmployeeSchedule/EmployeeScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visot.EmployeeSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
