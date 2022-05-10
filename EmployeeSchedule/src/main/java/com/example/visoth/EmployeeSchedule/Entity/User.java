592c5ea083c8f4cfb35544fc3c673abadd0f6337
Visoth <blacksterteoz@gmail.com>
2022-05-09 18:11:36 -0700

    -edited and polished the calendar page with javascript -shown database in the calendar page

---
+++ EmployeeSchedule/src/main/java/com/example/visoth/EmployeeSchedule/Entity/User.java
@@ -0,0 +1,109 @@
+package com.example.visoth.EmployeeSchedule.Entity;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH}
+    )
+    @JoinTable(
+            name="schedule_user",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name="schedule_id")
+    )
+    private Set<Schedule> schedules;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addSchedule(Schedule schedule){
+        if(schedules==null){
+            schedules=new HashSet<>();
+
+        }
+        schedules.add(schedule);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Schedule> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(Set<Schedule> schedules) {
+        this.schedules = schedules;
+    }
+}
---
+++ EmployeeSchedule/src/main/java/com/example/visoth/EmployeeSchedule/Service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.visoth.EmployeeSchedule.Service;
+
+import com.example.visoth.EmployeeSchedule.Entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> findALL();
+    public User findById(int id);
+    public void updateOrSave(User user);
+    public void deleteById(int id);
+}
